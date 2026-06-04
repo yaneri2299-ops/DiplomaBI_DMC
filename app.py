1b626c2303788b6cba4d7a0f676563b44121bf10
yaneri2299-ops <yaneri.2299@gmail.com>
2026-06-03 20:24:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,14 +12,16 @@ st.write("Elaborado por Yaneri Martinez")
 archivo = st.file_uploader("Cargue el archivo excel o csv")
 
 if archivo is not None :
+    
     if archivo.name.endswith(".csv"):
         data = pd.read_csv(archivo)
       st.write(data)
+
     elif archivo.name.endswith(".xlsx")
         data =pd.read_excel(archivo)
       st.write(data)
-else:
-  st.write("Formato no válido")
+    else:
+      st.write("Formato no válido")
 
 
 
