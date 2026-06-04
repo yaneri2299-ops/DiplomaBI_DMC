8f5cb3729fd94222c99966d3c93bd9324eeb7e1e
yaneri2299-ops <yaneri.2299@gmail.com>
2026-06-03 20:33:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,25 +5,25 @@ st.title("Proyecto Final Diploma BI")
 
 st.sidebar.title("Parámetros")
 
-st.image("python.png",width=500)
-st.sidebar.image("dmc.png",width=300)
+st.image("python.png", width=500)
+st.sidebar.image("dmc.png", width=300)
 
 st.write("Elaborado por Yaneri Martinez")
 
 archivo = st.file_uploader("Cargue el archivo excel o csv")
 
 if archivo is not None:
-    
+
     if archivo.name.endswith(".csv"):
         data = pd.read_csv(archivo)
-      st.write(data)
-    elif archivo.name.endswith(".xlsx")
+        st.write(data)
+
+    elif archivo.name.endswith(".xlsx"):
         data = pd.read_excel(archivo)
-      st.write(data)
+        st.write(data)
+
     else:
-      st.write("Formato no válido")
+        st.write("Formato no válido")
 
-
-else :
-    st.write ("Por favor cargue su archivo")
-  
+else:
+    st.write("Por favor cargue su archivo")
