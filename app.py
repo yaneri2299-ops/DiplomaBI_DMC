35b833a4206d30874b36f2adf233c5c9ea0d654c
yaneri2299-ops <yaneri.2299@gmail.com>
2026-06-03 20:12:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,21 @@ st.image("python.png",width=500)
 st.sidebar.image("dmc.png",width=300)
 
 st.write("Elaborado por Yaneri Martinez")
+
+archivo = st.file_uploader("Cargue el archivo excel o csv")
+
+if archivo is not None :
+    if archivo.mane.endswith(".csv"):
+        data = pd.read_csv(archivo)
+      st.write(data)
+    elif archivo.nameendswith(".xlsx")
+        data =pd.read_excel(archivo)
+      st.write(data)
+else:
+  st.write("Formato no válido")
+
+
+
+else :
+    st.write ("Por favor cargue su archivo")
+  
